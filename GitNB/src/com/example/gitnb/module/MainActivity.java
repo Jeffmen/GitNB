bd5605e8acb6100a018914cae04e98debdfef95f
10129255 <10129255@A23141889.zte.intra>
2015-11-09 17:26:06 +0800

    edit

--- GitNB/src/com/example/gitnb/module/MainActivity.java
+++ GitNB/src/com/example/gitnb/module/MainActivity.java
@@ -10,6 +10,8 @@ import com.example.gitnb.module.user.HotUserFragment;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -21,6 +23,8 @@ import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 
@@ -36,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setStatus();
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         TextView title = (TextView) toolbar.findViewById(R.id.title);
@@ -68,13 +73,28 @@ public class MainActivity extends AppCompatActivity {
 		tabs.setUnderlineHeight((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, dm));
 		tabs.setIndicatorHeight((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 2, dm));
 		tabs.setTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 16, dm));
-		tabs.setIndicatorColor(Color.parseColor("#4876FF"));
-		tabs.setSelectedTextColor(Color.parseColor("#4876FF"));
+		tabs.setIndicatorColor(Color.WHITE);
+		tabs.setSelectedTextColor(Color.WHITE);
 		//tabs.setBackgroundColor(ContextCompat.getColor(this,R.color.contacts_theme_color));
 		tabs.setTextColor(Color.BLACK);
 		tabs.setTabBackground(0);
 	}
 	
+    private void setStatus(){
+        //getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        if(VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            //window.setStatusBarColor(Color.TRANSPARENT);
+            window.setNavigationBarColor(Color.TRANSPARENT);
+        }
+    }
+    
     public class TabPagerAdapter extends FragmentPagerAdapter {
 
         private final List<Fragment> mFragments = new ArrayList<Fragment>();
